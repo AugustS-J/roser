0a747a7a6b2df968f84bd46c1803184d4466f2a9
AugustS-J <ausc0001@stud.kea.dk>
2023-08-29 09:25:28 +0200

    Opdateret JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// skriv din kode her.
+//skriv din kode her.
+window.addEventListener("load", start);
 
-// vælg alle blomster og add en click eventlistener til dem.
+//vælg alle blomster og add en click eventlistener til dem.
+function start() {
+  const roser = document.querySelectorAll("img");
+  roser.forEach((rose) => rose.addEventListener("click", pluk));
+}
 
-// lav en funktion der tilføjer class'en "pluk" til den blomst der bliver klikket på.
+//lav en funktion der tilføjer class'en "pluk" til den blomst der bliver klikket på.
+function pluk() {
+  //console.log(this);
+  this.classList.toggle("pluk");
+}
